38b5d20eeab860a0dc2053ba34a2b35efd7fb753
kacie granico <kgranico@gmail.com>
2018-10-12 14:12:39 -0700

    Initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+//Stateless Functional Component
+const Navbar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="navbar-brand">
+        Navbar
+        {'  '}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
